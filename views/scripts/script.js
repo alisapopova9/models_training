6c4814a15d47040e43ecf6306eabc88d4b8d1be4
diadolyy <niceline111@gmail.com>
2022-04-21 17:04:21 +0700

    work with buttons

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -38,3 +38,24 @@ form.addEventListener("submit", (event) => {
 function showBlock(){
     document.getElementById("hiddenBlock").hidden = false;
 };
+
+
+function ChangeLoadBtn(){
+    var checkInput = document.getElementById("file-upload");
+    if(checkInput.value){
+        document.getElementById("loadBtn").removeAttribute('disabled');
+        document.getElementById('fileHelpBlock').hidden=true;
+    }
+
+}
+
+function EnableBtns(){
+    event.preventDefault(); //убрать при отправке запроса
+    var checkInput = document.getElementById("file-upload");
+    document.getElementById('fileHelpBlock').hidden=false;
+    document.getElementById('fileHelpBlock').textContent=`Файл "${checkInput.value}" успешно загружен!`;
+    document.getElementById('watchDataBtn').removeAttribute('disabled');
+    document.getElementById('learnModelBtn').removeAttribute('disabled');
+
+}
+
